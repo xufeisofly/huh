32cee017b64f20f8757f3384da058f7449da19d5
xufei01 <xufei01@zhihu.com>
2019-11-08 18:39:34 +0800

    test data models

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/spf13/cast v1.3.0
 	google.golang.org/appengine v1.6.5 // indirect
+	gopkg.in/testfixtures.v2 v2.6.0
 )
--- orm_test.go
+++ orm_test.go
@@ -1,37 +1,14 @@
 package huh_test
 
 import (
-	"context"
-	"errors"
 	"os"
 	"testing"
 	"time"
 
+	model "github.com/xufeisofly/huh/testdata/models"
+
 	"github.com/xufeisofly/huh"
 )
-
-type User struct {
-	Email     string
-	ID        uint32    `huh:"pk"`
-	CreatedAt time.Time `huh:"readonly"`
-	UpdatedAt time.Time `huh:"readonly"`
-}
-
-func (u *User) TableName() string {
-	return "users"
-}
-
-func (u *User) BeforeCreate(ctx context.Context) error {
-	if u.ID == 2 {
-		return errors.New("before create error")
-	}
-	return nil
-}
-
-func (u *User) BeforeSave(ctx context.Context) error {
-	u.Email = "update3@huh.com"
-	return nil
-}
 
 func setup() {
 	huh.Config("mysql", huh.DBConfig{
@@ -71,10 +48,10 @@ func TestEverything(t *testing.T) {
 
 	o := huh.New()
 	ctx := huh.Context()
-	user := User{Email: "test@huh.com", ID: 1}
+	user := model.User{Email: "test@huh.com", ID: 1}
 
-	var receiver User
-	var receivers []User
+	var receiver model.User
+	var receivers []model.User
 
 	// test create raw sql
 	c, _ := o.Create().Of(ctx, &user)
@@ -83,51 +60,51 @@ func TestEverything(t *testing.T) {
 		t.Errorf("sqlStr actual: %s", sqlStr)
 	}
 
-	user = User{Email: "test@huh.com", ID: 1}
+	user = model.User{Email: "test@huh.com", ID: 1}
 	// test normal create
 	o.Create().Do(ctx, &user)
 
-	receiver = User{}
+	receiver = model.User{}
 	o.Get(1).Do(ctx, &receiver)
-	expected := User{Email: "update3@huh.com", ID: 1}
+	expected := model.User{Email: "update3@huh.com", ID: 1}
 
 	if receiver.Email != expected.Email {
 		t.Errorf("[create error] expect: %v, actual: %v", expected, receiver)
 	}
 
 	// test create with hooks
-	user2 := User{ID: 2, Email: "test2@huh.com"}
+	user2 := model.User{ID: 2, Email: "test2@huh.com"}
 	if err := o.Create().Do(ctx, &user2); err == nil {
 		t.Errorf("CreateBefore hook should have error")
 	}
 
-	receiver = User{}
+	receiver = model.User{}
 	o.Get(2).Do(ctx, &receiver)
-	if (User{}) != receiver {
+	if (model.User{}) != receiver {
 		t.Errorf("[get error] should get nil, actual: %v", receiver)
 	}
 
 	// test update
 	o.Update("email", "update@huh.com").Do(ctx, &user)
 
-	receiver = User{}
+	receiver = model.User{}
 	o.Get(user.ID).Do(ctx, &receiver)
 	if receiver.Email != "update@huh.com" {
 		t.Errorf("[update error] expected: %s, actual: %s", "update@huh.com", receiver.Email)
 	}
 
 	// test update_all with where
-	user4 := User{ID: 4, Email: "update@huh.com"}
+	user4 := model.User{ID: 4, Email: "update@huh.com"}
 	o.Create().Do(ctx, &user4)
 
-	receivers = []User{}
+	receivers = []model.User{}
 	o.Where("email = ?", "update3@huh.com").Do(ctx, &receivers)
 	if len(receivers) != 1 {
 		t.Errorf("[where error] result count should be 1")
 	}
 
-	receivers = []User{}
-	o.Where("email = ?", "update@huh.com").Update("email", "update2@huh.com").Do(ctx, User{})
+	receivers = []model.User{}
+	o.Where("email = ?", "update@huh.com").Update("email", "update2@huh.com").Do(ctx, model.User{})
 	o.Where("email = ?", "update@huh.com").Do(ctx, &receivers)
 
 	if len(receivers) != 0 {
@@ -135,7 +112,7 @@ func TestEverything(t *testing.T) {
 	}
 
 	// test transaction no create
-	user3 := User{ID: 3, Email: "test3@huh.com"}
+	user3 := model.User{ID: 3, Email: "test3@huh.com"}
 	o.Transaction(ctx, func(h *huh.Orm) {
 		h.Create().Do(ctx, &user3)
 		h.MustCreate().Do(ctx, &user)
@@ -150,23 +127,23 @@ func TestEverything(t *testing.T) {
 	}
 
 	// test get by pk
-	user5 := User{}
-	expected = User{ID: uint32(1), Email: "update2@huh.com"}
+	user5 := model.User{}
+	expected = model.User{ID: uint32(1), Email: "update2@huh.com"}
 	o.Get(1).Do(ctx, &user5)
 	if user5.Email != expected.Email {
 		t.Errorf("get error, expected: %v, actual: %v", expected, user5)
 	}
 
 	// test get by condition
-	user6 := User{}
-	expected = User{ID: uint32(4), Email: "update3@huh.com"}
+	user6 := model.User{}
+	expected = model.User{ID: uint32(4), Email: "update3@huh.com"}
 	o.GetBy("email", "update3@huh.com", "id", 4).Do(ctx, &user6)
 	if user6.Email != expected.Email {
 		t.Errorf("get error, expected: %v, actual: %v", expected, user6)
 	}
 
-	user7 := User{}
-	expected = User{ID: uint32(1), Email: "update2@huh.com"}
+	user7 := model.User{}
+	expected = model.User{ID: uint32(1), Email: "update2@huh.com"}
 	o.GetBy(map[string]interface{}{
 		"id":    1,
 		"email": "update2@huh.com",
@@ -176,9 +153,9 @@ func TestEverything(t *testing.T) {
 	}
 
 	// test where
-	var users = []User{}
+	var users = []model.User{}
 	o.Where("email = ?", "update3@huh.com").Do(ctx, &users)
-	expects := []User{
+	expects := []model.User{
 		{Email: "update3@huh.com", ID: 3},
 		{Email: "update3@huh.com", ID: 4},
 	}
@@ -189,17 +166,17 @@ func TestEverything(t *testing.T) {
 	}
 
 	// test limit offset
-	users = []User{}
+	users = []model.User{}
 	o.Where("email = ?", "update3@huh.com").Limit(1).Offset(1).Do(ctx, &users)
-	expected = User{Email: "update3@huh.com", ID: 4}
+	expected = model.User{Email: "update3@huh.com", ID: 4}
 	if expected.Email != users[0].Email {
 		t.Errorf("where error, expected: %v, actual: %v", expected, users[0])
 	}
 
 	// test order by
-	users = []User{}
+	users = []model.User{}
 	o.Where("email = ?", "update3@huh.com").Order("id desc").Do(ctx, &users)
-	expects = []User{
+	expects = []model.User{
 		{Email: "update3@huh.com", ID: 4},
 		{Email: "update3@huh.com", ID: 3},
 	}
@@ -210,27 +187,27 @@ func TestEverything(t *testing.T) {
 	}
 
 	// test destroy
-	user = User{}
+	user = model.User{}
 	o.Get(1).Do(ctx, &user)
 	o.Destroy().Do(ctx, &user)
 
-	user = User{}
+	user = model.User{}
 	o.GetBy("email", "update2@huh.com").Do(ctx, &user)
-	if (User{}) != user {
+	if (model.User{}) != user {
 		t.Errorf("[destroy error], destroy failed")
 	}
 
 	// test destroy where
-	o.Where("email = ?", "update3@huh.com").Destroy().Do(ctx, User{})
-	users = []User{}
+	o.Where("email = ?", "update3@huh.com").Destroy().Do(ctx, model.User{})
+	users = []model.User{}
 	o.Where("email = ?", "update3@huh.com").Do(ctx, &users)
 	if len(users) != 0 {
-		t.Errorf("[destroy error], destroy failed, expected: %v, actual: %v", []User{}, users)
+		t.Errorf("[destroy error], destroy failed, expected: %v, actual: %v", []model.User{}, users)
 	}
 
 	// test nested transaction
-	user8 := User{ID: 8, Email: "test8@huh.com"}
-	user9 := User{ID: 9, Email: "test9@huh.com"}
+	user8 := model.User{ID: 8, Email: "test8@huh.com"}
+	user9 := model.User{ID: 9, Email: "test9@huh.com"}
 	o.Transaction(ctx, func(o *huh.Orm) {
 		o.Create().Do(ctx, &user8)
 		// user2 cannot be created
@@ -243,7 +220,7 @@ func TestEverything(t *testing.T) {
 		})
 	})
 
-	users = []User{}
+	users = []model.User{}
 	o.Where("email = 'update3@huh.com'").Do(ctx, &users)
 
 	if users[0].ID != 8 || users[1].ID != 9 {
@@ -251,27 +228,27 @@ func TestEverything(t *testing.T) {
 	}
 
 	// test multi where
-	users = []User{}
+	users = []model.User{}
 	o.Where("email", "update3@huh.com").And("id > ?", 8).Do(ctx, &users)
 	if len(users) != 1 {
 		t.Errorf("[multi where error] users should be 1")
 	}
 
 	// test selected columns
-	user = User{}
+	user = model.User{}
 	o.Select("id", "created_at").GetBy("email", "update3@huh.com").Do(ctx, &user)
 	if user.Email != "" || user.ID != 8 {
 		t.Errorf("[select error] user email expected: %s, actual: %s; user id expected: %d, actual: %d", "", user.Email, 8, user.ID)
 	}
 
-	users = []User{}
+	users = []model.User{}
 	o.Where("id IN ?", []interface{}{8, 9}).Do(ctx, &users)
 	if len(users) != 2 {
 		t.Errorf("[where in error] users length should be 2")
 	}
 
 	// test where or
-	users = []User{}
+	users = []model.User{}
 	o.Where("id", 9).Or("id", 8).Do(ctx, &users)
 	if len(users) != 2 {
 		t.Errorf("[where or error] users should be 2")
@@ -279,28 +256,28 @@ func TestEverything(t *testing.T) {
 }
 
 func BenchmarkSelect(b *testing.B) {
-	var users []User
+	var users []model.User
 	for i := 0; i < b.N; i++ {
 		huh.New().Where("id", 9).Or("id", 8).Of(huh.Context(), &users)
 	}
 }
 
 func BenchmarkCreate(b *testing.B) {
-	var user = User{}
+	var user = model.User{}
 	for i := 0; i < b.N; i++ {
 		huh.New().Create().Of(huh.Context(), &user)
 	}
 }
 
 func BenchmarkUpdate(b *testing.B) {
-	var user = User{ID: 1}
+	var user = model.User{ID: 1}
 	for i := 0; i < b.N; i++ {
 		huh.New().Update().Of(huh.Context(), &user)
 	}
 }
 
 func BenchmarkDestroy(b *testing.B) {
-	var user = User{ID: 1}
+	var user = model.User{ID: 1}
 	for i := 0; i < b.N; i++ {
 		huh.New().Destroy().Of(huh.Context(), &user)
 	}
